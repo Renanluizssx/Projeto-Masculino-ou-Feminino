b0e677b914c41a4c27c7667f15508ef250b36aea
Renan Luiz <renanluizssx@gmail.com>
2022-08-22 01:21:29 -0300

    comitado

--- script/script.js
+++ script/script.js
@@ -50,7 +50,7 @@ function verificar() {
         }
 
     }
-    res.style.textAlign = 'center';
+    
     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     res.appendChild(img)
     }
